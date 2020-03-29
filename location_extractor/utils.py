8f99d15f6af60fad40497a179618eeb5340470ac
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2020-03-29 17:07:57 +0200

    move helpers to utils and adjust it to wemake-python-styleguide

--- location_extractor/utils.py
+++ location_extractor/utils.py
@@ -1,5 +1,15 @@
 import jellyfish
 
+from unidecode import unidecode_expect_nonascii
 
-def fuzzy_match(s1, s2, max_dist=6):
-    return jellyfish.levenshtein_distance(s1, s2) <= max_dist
+
+def fuzzy_match(text1: str, text2: str, max_dist: int = 6) -> bool:
+    return jellyfish.levenshtein_distance(text1, text2) <= max_dist
+
+
+def remove_accents(place: str) -> str:
+    return unidecode_expect_nonascii(place)
+
+
+def parse_query_param(query_param: str) -> str:
+    return remove_accents(query_param.lower()).replace("'", r"\'")
