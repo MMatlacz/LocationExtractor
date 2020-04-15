f437b1f57cfcbac40f1822163b704be1aa2f2e4b
Marcin Matłacz <martin.matlacz@gmail.com>
2020-04-15 11:02:34 +0200

    Add spacy, remove nltk;

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,7 @@ import pytest
 
 from location_extractor.clients import DBClient
 from location_extractor.named_entity_recognition.ner import NERExtractor
-from location_extractor.named_entity_recognition.geograpy_nltk import download_nltk
 from location_extractor.extractor import Extractor
-
-download_nltk()
 
 
 @pytest.fixture(scope='session')
